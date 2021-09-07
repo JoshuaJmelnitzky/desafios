6d183715c34d1f53b93a15631ce6fbc6a2e1f59f
Joshua Jmelnitzky <joshuajmelnitzkysj@gmail.com>
2021-09-07 15:06:21 -0300

    desafio 2

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 // Ingreso y muestra de nombre y apellido
+let nombreValidar = "Joshua"
+let apellidoValidar = "Jmelnitzky"
 let nombre = prompt("Ingrese su nombre: ");
 let apellido = prompt("Ingrese su apellido: ");
 const blanco = " ";
 
 let nombreCompleto = "Bienvenido" + blanco + nombre + blanco + apellido;
 
-console.log(nombreCompleto);
+if (nombre != "" && apellido != "" && nombreValidar == nombre && apellidoValidar == apellido){
+    console.log(nombreCompleto);
+}
+else if(nombre != nombreValidar || apellido != apellidoValidar){
+    alert("No se encontró el usuario ingresado");
+}
+else{
+    alert("No se ha introducido ningún nombre/apellido");
+}
 
 // Ingreso y muestra de edad
 let edad = prompt("Ingrese su edad: ");
@@ -22,9 +32,49 @@ console.log(edadPromTxt)
 
 // Ingreso y muestra de calculo de salario 
 let artVendidos = prompt("Ingrese la cantidad de artículos vendidos: ");
+let sector = prompt("Ingrese en que sector trabaja: ");
 let sueldoBase = parseFloat(35500.85);
 let comision = parseFloat(5224.54);
 
+
+if (sector == "administrativo" || sector == "Administrativo"){
+    sueldoBase += 10000;
+}
+else if(sector == "RRHH" || sector == "rrhh"){
+    sueldoBase += 8000;
+}
+else if(sector == "ventas" || sector == "Ventas"){
+    sueldoBase += 7000;
+}
+else{
+    sueldoBase += 2000;
+}
+
+
 let calculoSalario = sueldoBase + artVendidos * comision;
 
+console.log("Su cargo es: " + sector);
 console.log("Su sueldo es de " + "$" + calculoSalario);
+
+// Oferta artículo
+
+let precio = parseFloat(prompt("Introducir precio de un artículo"));
+
+if (precio > 0 && precio < 3000){
+    console.log("El producto se encuentra en oferta");
+}
+else if (precio > 3000 && precio < 5000){
+    console.log("El producto se encuentra a precio regular");
+}
+else if (precio < 0){
+    alert("Un precio no puede ser negativo");
+}
+else{
+    console.log("El producto se encuentra en sobreprecio");
+}
+
+
+
+
+
+   
